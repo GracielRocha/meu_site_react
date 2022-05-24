479def7cfdd3fae3a5dd54ee6fbef083b187835e
GracielRocha <gracielguerreiro@gmail.com>
2022-05-23 22:54:50 -0300

    Primeiro commit

---
+++ src/componentes/Hello.js
@@ -0,0 +1,14 @@
+function Hello() {
+    
+return(
+    <div>
+
+    <h1>Hello devs da DIO!</h1>
+    <p>Vamos transformar estudo em desenvolvimento</p>
+    <img src="http://https://img.estadao.com.br/thumbs/640/resources/jpg/7/9/1530138434297.jpg"></img>
+    
+    </div>
+)
+
+}
+export default Hello
---
+++ src/componentes/Hellodio.js
@@ -0,0 +1,13 @@
+function HelloDio(props){
+
+
+ return (
+        <div>
+            <h3>
+                E ai Turma do transformatec
+            </h3>
+        </div>
+    )
+}
+export default HelloDio
+   
